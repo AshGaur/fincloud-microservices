eb6ed55f3af6c3cf980463938919bbe4b04e65a3
Ashutosh <14121792+AshGaur@users.noreply.github.com>
2025-06-11 07:08:23 +0530

    Circuit Breaker pattern with Resilience4j library. * Gateway Circuit breakers and fallbacks. * Open feign circuit breakers and fallbacks added.

---
+++ accounts/src/main/java/com/flashstack/accounts/service/client/LoansFallback.java
@@ -0,0 +1,13 @@
+package com.flashstack.accounts.service.client;
+
+import com.flashstack.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient {
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
