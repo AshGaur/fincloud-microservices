0fed15fbeefb3b9b1aee69b8c967a7052ef08afd
Ashutosh <14121792+AshGaur@users.noreply.github.com>
2025-06-21 01:20:30 +0530

    Resiliency Patterns RateLimiter * Redis rate limiter on Cards microservice with Gateway Server. * Resiliency4j Rate Limiter for Accounts Microservice using annotation @RateLimiter.

--- gatewayserver/src/main/java/com/flashstack/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/flashstack/gatewayserver/GatewayserverApplication.java
@@ -2,10 +2,13 @@ package com.flashstack.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -36,6 +39,10 @@ public class GatewayserverApplication {
 						.filters(f -> f
 								.rewritePath("/flashstack/cards/(?<segment>)", "/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config
+										.setRateLimiter(this.redisRateLimiter())
+										.setKeyResolver(this.userKeyResolver())
+								)
 						)
 						.uri("lb://CARDS")
 				)
@@ -53,5 +60,17 @@ public class GatewayserverApplication {
 						.uri("lb://LOANS")
 				)
 				.build();
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1,1,1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(
+				exchange.getRequest().getHeaders().getFirst("user")
+		).defaultIfEmpty("anonymous");
 	}
 }
